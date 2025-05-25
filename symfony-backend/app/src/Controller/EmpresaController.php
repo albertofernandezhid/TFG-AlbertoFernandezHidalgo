0130a2a78636f92d1bfdcb4b1ab2340b649f9869
FranInfante <infante.portero@gmail.com>
2025-05-25 13:03:01 +0200

    empresa crear endpoint

--- symfony-backend/app/src/Controller/EmpresaController.php
+++ symfony-backend/app/src/Controller/EmpresaController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Empresa;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class EmpresaController extends AbstractController
 {
-    #[Route('/empresa', name: 'app_empresa')]
-    public function index(): JsonResponse
+
+     #[Route('/empresa', name: 'crear_empresa', methods: ['POST'])]
+    public function crear(Request $request, EntityManagerInterface $em): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/EmpresaController.php',
-        ]);
+        $data = json_decode($request->getContent(), true);
+
+        $empresa = new Empresa();
+        $empresa->setNombre($data['nombreEmpresa']);
+        $empresa->setCif($data['cif']);
+        $empresa->setEmailAdmin($data['email']);
+        $empresa->setTelefono($data['telefono']);
+        $empresa->setPassAdmin($data['contrasenaAdmin']);
+        $empresa->setCreatedAt(new \DateTimeImmutable());
+
+        $em->persist($empresa);
+        $em->flush();
+
+        return $this->json(['message' => 'Empresa creada con éxito'], 201);
     }
 }
