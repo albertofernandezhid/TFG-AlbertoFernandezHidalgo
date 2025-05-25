0dd36a8ff74fd8ce9a229b8f1a44d50c11c391aa
FranInfante <infante.portero@gmail.com>
2025-05-25 13:47:05 +0200

    removed options settings on index

--- symfony-backend/app/public/index.php
+++ symfony-backend/app/public/index.php
@@ -1,13 +1,6 @@
 <?php
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: http://localhost:4200');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
-    exit(0);
-}
-
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
